d6bde4d3ba2d19b0cdcaf23438fc4348fb94e99a
The Billionare <kdcp999@ubuntu.ubuntu-domain>
2012-10-17 21:16:54 +0100

    initial app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corazon::Application.config.secret_token = 'd3df6f6015f95ac88c4370f189e59ffb220c0bfbedca4a430af42958365681c6683b21ca8704aa94d9f5d6e1e67707691f260d7415cdec4035b744259df8b50c'
